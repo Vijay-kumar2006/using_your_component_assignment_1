46ed23c5a019901cc94b91c307b1c1f38fe4738e
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-03-04 12:56:46 +0530

    Update bookcard.jsx

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const BookCard=({book})=>{
   return(
     <div>
-      <img src="{book.image}"/>
-      <h2><b>author:</b> {book.name}</h2>
-      <p><b>author:</b> {book.genre}</p> 
+      <img src={book.image}/>
+      <h2><b>name:</b> {book.name}</h2>
+      <p><b>genre:</b> {book.genre}</p> 
       <p><b>author:</b> {book.author}</p>
 
     </div>
